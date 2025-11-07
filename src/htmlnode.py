0a4ab74174d5e1a7b11f3d59f080549711b70faa
Imistake <kevinseocalm@gmail.com>
2025-11-07 13:09:39 +0000

    feat: Implement HTMLNode class

---
+++ src/htmlnode.py
@@ -0,0 +1,24 @@
+
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        
+        attributes = []
+
+        for prop_name, prop_value in self.props.items():
+            attributes.append(f'{prop_name}="{prop_value}"')
+        
+        return " " + " ".join(attributes)
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
