eddd28c3176ff92640634d3eb5e5eb6d12116a75
Imistake <kevinseocalm@gmail.com>
2025-11-07 12:10:58 +0000

    feat: Implement TextNode class and initial project setup

---
+++ src/textnode.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        if isinstance(text_type, TextType):
+            normalized = text_type
+        elif isinstance(text_type, str):
+            normalized = TextType(text_type)
+        else:
+            raise TypeError("text_type must be a TextType or valid string value")
+        self.text_type = normalized
+        self.url = url
+
+    def __eq__(self, value):
+        if isinstance(value, TextNode):
+            return self.text == value.text and self.text_type == value.text_type and self.url == value.url 
+        else:
+            return False
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
