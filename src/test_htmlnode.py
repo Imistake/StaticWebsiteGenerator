dfa8c6d3fb5b87d0198a89f5df423c2fffd24f29
Imistake <kevinseocalm@gmail.com>
2025-11-10 16:13:41 +0000

    feat: add ability to create TextNodes from raw markdown strings; move tests to their files

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -122,58 +122,5 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIn('id="x"', html)
         self.assertTrue(html.endswith("</div>"))
 
-    def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
-
-    def test_bold_text(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "b")
-        self.assertEqual(html_node.value, "This is a text node")
-
-    def test_italic_text(self):
-        node = TextNode("This is a text node", TextType.ITALIC)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "i")
-        self.assertEqual(html_node.value, "This is a text node")
-    
-    def test_code_text(self):
-        node = TextNode("This is a text node", TextType.CODE)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "code")
-        self.assertEqual(html_node.value, "This is a text node")
-
-    def test_links(self):
-        node = TextNode("Boot.dev", TextType.LINK, "https://boot.dev")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "a")
-        self.assertEqual(html_node.value, "Boot.dev")
-        self.assertIsNotNone(html_node.props)
-        self.assertEqual(html_node.props.get("href"), "https://boot.dev")
-    
-    def test_images(self):
-        node = TextNode("A cat", TextType.IMAGE, "https://ex.com/cat.png")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "img")
-        self.assertEqual(html_node.value, "")
-        self.assertIsNotNone(html_node.props)
-        self.assertEqual(html_node.props.get("src"), "https://ex.com/cat.png")
-        self.assertEqual(html_node.props.get("alt"), "A cat")
-
-    def test_unsupported_type_raises(self):
-        node = TextNode("x", TextType.TEXT)
-        node.text_type = None
-        with self.assertRaises(ValueError):
-            text_node_to_html_node(node)
-
-    def test_plain_has_no_props(self):
-        node = TextNode("test node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertIsNone(html_node.props)
-
-
 if __name__ == "__main__":
     unittest.main()
