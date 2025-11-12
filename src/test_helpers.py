101915a71932d6aa6081e09aec4e9dfe18b18ada
Imistake <kevinseocalm@gmail.com>
2025-11-12 13:18:55 +0000

    feat: add converting raw string of markdown to TextNode objects; add tests to test_helpers.py

--- src/test_helpers.py
+++ src/test_helpers.py
@@ -5,6 +5,7 @@ from helpers import (
     split_nodes_link,
     extract_markdown_images,
     extract_markdown_links,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
@@ -199,6 +200,50 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_split_everything(self):
+        
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_textnodes(text)
+        expected = [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(expected, result)
+
+    
+    def test_unmatched_italic_is_plain_text(self):
+        text = "lone _"
+        with self.assertRaises(ValueError):
+            text_to_textnodes(text)
+
+    def test_unmatched_bold_is_plain_text(self):
+        text = "**"
+        with self.assertRaises(ValueError):
+            text_to_textnodes(text)
+
+    def test_incomplete_link_is_plain_text(self):
+        text = "[x](missing"
+        self.assertEqual(
+            [TextNode("[x](missing", TextType.TEXT)],
+            text_to_textnodes(text),
+        )
+
+    def test_incomplete_image_is_plain_text(self):
+        text = "![alt]("
+        self.assertEqual(
+            [TextNode("![alt](", TextType.TEXT)],
+            text_to_textnodes(text),
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
